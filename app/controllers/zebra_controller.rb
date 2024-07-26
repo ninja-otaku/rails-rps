405c2d041fad5902b4316bfa126980e2d0958805
ninja-otaku <nsgowthamchakkravarthy@gmail.com>
2024-07-26 00:50:21 +0000

    Pending changes exported from your codespace

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,17 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @random_move =['rock','paper','scissors'].sample
+    render({:template=> "game_templates/play_rock"})
+  end
+  def elephant
+    @random_move =['rock','paper','scissors'].sample
+    render({:template=> "game_templates/play_paper"})
+  end
+  def lion
+    @random_move =['rock','paper','scissors'].sample
+    render({:template=> "game_templates/play_scissors"})
+  end
+  def rules
+    render({:template=> "game_templates/rules"})
+  end
+end
